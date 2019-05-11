b18be8b46c1bd864f35436c18073b1540075fabb
Raian Moretti <48537653+Raian-Moretti@users.noreply.github.com>
2019-05-11 20:05:33 -0300

    Update Player.h

--- src/player/Player.h
+++ src/player/Player.h
@@ -1,3 +1,15 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <ctime>
+#include <ratio>
+#include <chrono>
+#include <string>
+#include <string.h>
+#include <unistd.h>
+
+using namespace std;
+using namespace chrono;
+
 class Player{
     private:
         string name;
@@ -9,10 +21,12 @@ class Player{
 
         void move(sf::Vector2f vec){
             player.setPosition({player.getPosition().x + vec.x, player.getPosition().y + vec.y});
-            if(player.getPosition().x > 100)
+            if(player.getPosition().x - (view->getCenter().x+((view->getSize().x)/2))  > -50   && vec.x > 0)
                 view->move({vec.x,0});
-            else if(player.getPosition().x < 100)
+            if(player.getPosition().x - (view->getCenter().x+((view->getSize().x)/2))  < -550 && vec.x < 0)
                 view->move({vec.x,0});
+
+
         }
     public:
         Player(string _name = ""):
@@ -26,7 +40,7 @@ class Player{
         void drawTo(sf::RenderWindow &window) {
             window.draw(player);
         }
- 
+
         void setPos(sf::Vector2f newPos) {
             player.setPosition(newPos);
         }
@@ -36,6 +50,15 @@ class Player{
         }
         void moveLeft(){
             move({-moveSpeed,0});
+        }
+        void moveUp(){
+
+            for(int i=0; i<5;i++)
+                move({0,-moveSpeed});
+        }
+        void moveDown(){
+            for(int i=5; i>0;i--)
+                move({0,moveSpeed});
         }
 
         void setView(sf::View* _view){

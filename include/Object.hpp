44ea806d8621d6aeefb3fc610b9e74cf6b4e6062
Raian-Moretti <raian.moretti.eletro@gmail.com>
2020-01-30 13:34:02 -0200

    Remove duplicate directories

--- include/Object.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "includes.hpp"
-class Object{
-    public:
-        virtual void fall(){}
-        virtual sf::FloatRect getRect(){ return {0,0,0,0}; }
-        Object();
-        ~Object();
-};
